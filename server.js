95ba68f43c56fa88a7c9dedc85dfb2c80dc93399
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-09-07 20:18:01 -0400

    updated server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  process.env.MONGODB_URI || "mongodb+srv://Jonayah:seMDz^^Kg97B@cluster0.rs5op.mongodb.net/library?retryWrites=true&w=majority",
   {
     useCreateIndex: true,
     useNewUrlParser: true
